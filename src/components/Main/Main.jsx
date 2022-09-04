642e3ba2fb405d7e6e9c1538060ac4a5c89c04a9
shakoor123 <shakoorchachu2001@gmail.com>
2022-09-04 17:00:11 +0530

    main started

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./Main.scss";
 
 function Main() {
-  return <div className="main">Main</div>;
+  return (
+    <div className="main">
+      <div className="mainWrapper">
+        <div className="mainContent">
+          <div className="mainleft">l</div>
+          <div className="mainRight">r</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
