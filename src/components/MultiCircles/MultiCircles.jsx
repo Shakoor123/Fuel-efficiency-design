b236a902bedfb75f1660b238d30e1123cb10a7ce
shakoor123 <shakoorchachu2001@gmail.com>
2022-09-06 11:37:45 +0530

    half design completed

--- src/components/MultiCircles/MultiCircles.jsx
+++ src/components/MultiCircles/MultiCircles.jsx
@@ -1,21 +1,59 @@
 import React from "react";
 import "./MultiCircles.scss";
-
+import CicularProgressBar from "../CicularProgressBar/CicularProgressBar";
 const MultiCircles = () => {
   return (
     <div className="mul">
       <div className="rows">
         <div className="row">
-          <div className="colum">.</div>
-          <div className="colum">.</div>
-          <div className="colum">.</div>
-          <div className="colum">.</div>
+          <div className="colum leftBorder">
+            <div className="circle">
+              <CicularProgressBar />
+            </div>
+          </div>
+          <div className="colum leftBorder">
+            <div className="circle">
+              <CicularProgressBar />
+            </div>
+          </div>
+          <div className="colum leftBorder">
+            <div className="circle">
+              <CicularProgressBar />
+            </div>
+          </div>
+          <div className="colum leftBorder">
+            <div className="circle">
+              <CicularProgressBar />
+            </div>
+          </div>
         </div>
         <div className="row">
-          <div className="colum">.</div>
-          <div className="colum">.</div>
-          <div className="colum">.</div>
-          <div className="colum">.</div>
+          <div className="colum topBorder "></div>
+          <div className="colum topBorder leftBorder"></div>
+          <div className="colum topBorder leftBorder">
+            <div className="circle">
+              <CicularProgressBar />
+            </div>
+          </div>
+          <div className="colum leftBorder"></div>
+        </div>
+        <div className="row">
+          <div className="colum"></div>
+          <div className="colum ">
+            <div className="circle">
+              <CicularProgressBar />
+            </div>
+          </div>
+          <div className="colum ">
+            <div className="circle">
+              <CicularProgressBar />
+            </div>
+          </div>
+          <div className="colum">
+            <div className="circle">
+              <CicularProgressBar />
+            </div>
+          </div>
         </div>
       </div>
     </div>
